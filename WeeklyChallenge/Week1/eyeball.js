697f086f50fc626bf7855b6129fd86c5449052ac
BUNN1E5 <goldenneedham@gmail.com>
2019-02-14 01:58:27 -0800

    Made faces into eyes

--- WeeklyChallenge/Week1/eyeball.js
+++ WeeklyChallenge/Week1/eyeball.js
@@ -32,10 +32,10 @@ function start(){
     var cen = {x:c.canvas.width/2, y:c.canvas.height/2}
 
 
-    eyes = [new Eyes({x:cen.x , y:cen.y }, size, 10, 10)];
+    eyes = [];
 
     for(var i = 0; i < 10; i++){
-        eyes.push(new Eyes({x:c.canvas.width * Math.random(), y:c.canvas.height * Math.random()}, size, 10, 10));
+        eyes.push(new Eyes({x:c.canvas.width * Math.random(), y:c.canvas.height * Math.random()}, size, 10, 20));
     }
 
 }
@@ -65,6 +65,40 @@ class Eyes{
     }
 
     drawEyes(){
+        var diff_x = this.position.x - mousePos.x;
+        var diff_y = this.position.y - mousePos.y;
+
+        var r = Math.sqrt(diff_y * diff_y + diff_x * diff_x);
+        var angle = Math.atan2(mousePos.y - this.position.y, mousePos.x - this.position.x);
+        c.beginPath();
+        c.save();
+        var path = new Path2D("m -92.339873,0.29025539 c 56.02747,-61.03891739 137.820856,-60.95338539 193.769803,0 -55.975077,60.98184261 -137.735802,61.04603261 -193.769803,0 z");
+
+        c.translate(this.position.x, this.position.y);
+
+        c.lineWidth = 10;
+        c.fillStyle = "#000000";
+        c.fill(path);
+
+        c.beginPath();
+        c.fillStyle = "#FF0000";
+        r/=40;
+        r = r > 1 ? 1 : r;
+
+        c.arc(r * this.look_size * Math.cos(angle) * 2, r * this.look_size * Math.sin(angle), this.size * 10, 0, 2 * Math.PI);
+        c.fill()
+
+        c.lineWidth = 10;
+        c.strokeStyle = "#FFFFFF";
+        c.stroke(path);
+
+
+        c.restore();
+
+
+    }
+
+    drawHeads(){
         var angle = Math.atan2(mousePos.y - this.position.y, mousePos.x - this.position.x);
 
         c.beginPath();
